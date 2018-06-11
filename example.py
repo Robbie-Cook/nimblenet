8d713cf135d3a3e1bfe20984dc6945bbe66a3324
Robbie-Cook <robbieck7@gmail.com>
2018-06-12 02:30:04 +1200

    Last commit before bed

--- example.py
+++ example.py
@@ -5,14 +5,18 @@ from nimblenet.neuralnet import NeuralNet
 from nimblenet.preprocessing import construct_preprocessor, standarize
 from nimblenet.data_structures import Instance
 from nimblenet.tools import *
-import interference
 import task
 import os
 import json
 
+
+
+
 """
 Parameters:
 """
+
+
 
 inputNodes = 32
 hiddenNodes = 16
@@ -29,6 +33,59 @@ printRate = 5000
 learningAlgorithm = backpropagation_classical_momentum
 repeats = 50
 
+
+def intervene(interveningTask):
+    print("\n\nIntervening\n")
+    interveningInputs = interveningTask['inputPatterns']
+    interveningTeacher = interveningTask['teacher']
+
+    interveningDataset = []
+    for i in range(len(interveningInputs)):
+        interveningDataset.append(Instance(interveningInputs[i], interveningTeacher[i]))
+
+    # Train the network using backpropagation
+    learningAlgorithm(
+            network,                            # the network to train
+            interveningDataset,                      # specify the training set
+            interveningDataset,                          # specify the test set
+            cost_function,                      # specify the cost function to calculate error
+
+            ERROR_LIMIT             = errorCriterion,     # define an acceptable error limit
+            max_iterations         = 10000,      # continues until the error limit is reach if this argument is skipped
+
+            batch_size              = batch_size,        # 1 := no batch learning, 0 := entire trainingset as a batch, anything else := batch size
+            print_rate              = printRate,     # print error status every `print_rate` epoch.
+            learning_rate           = learningConstant,      # learning rate
+            momentum_factor         = momentumConstant,      # momentum
+            input_layer_dropout     = 0.0,      # dropout fraction of the input layer
+            hidden_layer_dropout    = 0.0,      # dropout fraction in all hidden layers
+            save_trained_network    = False     # Whether to write the trained weights to disk
+        )
+
+def recency(newTask):
+    print("\n\nRecency\n")
+    buffer = [Instance(newTask['inputPatterns'][0],
+                dataset[-3],
+                dataset[-2],
+                dataset[-1]]
+    # Train the network using backpropagation
+    learningAlgorithm(
+            network,                            # the network to train
+            interveningDataset,                      # specify the training set
+            interveningDataset,                          # specify the test set
+            cost_function,                      # specify the cost function to calculate error
+
+            ERROR_LIMIT             = errorCriterion,     # define an acceptable error limit
+            max_iterations         = 10000,      # continues until the error limit is reach if this argument is skipped
+
+            batch_size              = batch_size,        # 1 := no batch learning, 0 := entire trainingset as a batch, anything else := batch size
+            print_rate              = printRate,     # print error status every `print_rate` epoch.
+            learning_rate           = learningConstant,      # learning rate
+            momentum_factor         = momentumConstant,      # momentum
+            input_layer_dropout     = 0.0,      # dropout fraction of the input layer
+            hidden_layer_dropout    = 0.0,      # dropout fraction in all hidden layers
+            save_trained_network    = False     # Whether to write the trained weights to disk
+        )
 
 i = 0
 while "output{}.txt".format(i) in os.listdir("."):
@@ -107,33 +164,7 @@ for i in range(repeats):
     """
     Intervening trial
     """
-    def intervene(interveningTask):
-        print("\n\n\nIntervening\n\n\n")
-        interveningInputs = interveningTask['inputPatterns']
-        interveningTeacher = interveningTask['teacher']
 
-        interveningDataset = []
-        for i in range(len(interveningInputs)):
-            interveningDataset.append(Instance(interveningInputs[i], interveningTeacher[i]))
-
-        # Train the network using backpropagation
-        learningAlgorithm(
-                network,                            # the network to train
-                interveningDataset,                      # specify the training set
-                interveningDataset,                          # specify the test set
-                cost_function,                      # specify the cost function to calculate error
-
-                ERROR_LIMIT             = errorCriterion,     # define an acceptable error limit
-                max_iterations         = 10000,      # continues until the error limit is reach if this argument is skipped
-
-                batch_size              = batch_size,        # 1 := no batch learning, 0 := entire trainingset as a batch, anything else := batch size
-                print_rate              = printRate,     # print error status every `print_rate` epoch.
-                learning_rate           = learningConstant,      # learning rate
-                momentum_factor         = momentumConstant,      # momentum
-                input_layer_dropout     = 0.0,      # dropout fraction of the input layer
-                hidden_layer_dropout    = 0.0,      # dropout fraction in all hidden layers
-                save_trained_network    = False     # Whether to write the trained weights to disk
-            )
     for i in range(0, len(interventions)):
         intervene(interventions[i])
         outputFile.write(str(getError(network=network, testset=test_data, cost_function=cost_function))+"\n")
